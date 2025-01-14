7c057770364b62741fd22541fa82436b14b12d0f
Snezhana Pashovska <snezhana.pashovska@hotmail.com>
2025-01-14 14:04:49 +0100

    Commit-12

--- tests/Functional/Auth/RegisterTest.php
+++ tests/Functional/Auth/RegisterTest.php
@@ -18,7 +18,6 @@ final class RegisterTest extends FunctionalTestCase
         $this->client->submitForm('S\'inscrire', self::getFormData());
         self::assertResponseRedirects('/auth/login');
 
-
         $user = $this->getEntityManager()->getRepository(User::class)->findOneByEmail('user@email.com');
         $userPasswordHasher = $this->service(UserPasswordHasherInterface::class);
 
@@ -41,20 +40,20 @@ final class RegisterTest extends FunctionalTestCase
         self::assertResponseIsUnprocessable();
     }
 
-    /**
-     * Provides invalid form data with descriptions.
-     *
-     * @return iterable<string, array{0: array<string, string>}>
-     */
-    public static function provideInvalidFormData(): iterable
-    {
-        yield 'empty username' => [['register[username]' => '', 'register[email]' => 'user@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
-        yield 'non unique username' => [['register[username]' => 'user+1', 'register[email]' => 'user@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
-        yield 'too long username' => [['register[username]' => str_repeat('a', 256), 'register[email]' => 'user@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
-        yield 'empty email' => [['register[username]' => 'username', 'register[email]' => '', 'register[plainPassword]' => 'SuperPassword123!']];
-        yield 'non unique email' => [['register[username]' => 'username', 'register[email]' => 'user+1@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
-        yield 'invalid email' => [['register[username]' => 'username', 'register[email]' => 'fail', 'register[plainPassword]' => 'SuperPassword123!']];
-    }
+   /**
+ * Provides invalid form data with descriptions.
+ *
+ * @return iterable<string, array{0: array<string, string>}>
+ */
+public static function provideInvalidFormData(): iterable
+{
+    yield 'empty username' => [['register[username]' => '', 'register[email]' => 'user@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
+    yield 'non unique username' => [['register[username]' => 'user+1', 'register[email]' => 'user@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
+    yield 'too long username' => [['register[username]' => str_repeat('a', 256), 'register[email]' => 'user@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
+    yield 'empty email' => [['register[username]' => 'username', 'register[email]' => '', 'register[plainPassword]' => 'SuperPassword123!']];
+    yield 'non unique email' => [['register[username]' => 'username', 'register[email]' => 'user+1@email.com', 'register[plainPassword]' => 'SuperPassword123!']];
+    yield 'invalid email' => [['register[username]' => 'username', 'register[email]' => 'fail', 'register[plainPassword]' => 'SuperPassword123!']];
+}
 
     /**
      * @param array<string, string> $overrideData
@@ -63,9 +62,9 @@ final class RegisterTest extends FunctionalTestCase
     public static function getFormData(array $overrideData = []): array
     {
         return [
-            'register[username]' => 'ValidUsername',
-            'register[email]' => 'validemail@example.com',
-            'register[plainPassword]' => 'ValidPassword123!',
+            'register[username]' => 'username',
+            'register[email]' => 'user@email.com',
+            'register[plainPassword]' => 'SuperPassword123!',
         ] + $overrideData;
     }
 }
